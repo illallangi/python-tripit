5123a0fba4362865d7ce0eea660eb23a0d99f1e1
Andrew Cole <andrew.cole@illallangi.com>
2024-10-05 17:51:12 +1000

    fix(__version__.py): removed local part from version, added automatic stripping of local path when calculating tuple

--- src/illallangi/tripit/__version__.py
+++ src/illallangi/tripit/__version__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.1+e9a07d219d824c48adf93085f25b2e83"
+__version__ = "0.1.1"
 
 # Version tuple
-__version_info__ = tuple(map(int, __version__.split(".")))
+__version_info__ = tuple(map(int, __version__.split("+")[0].split(".")))
